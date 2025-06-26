5919060b8b20cdb1887bce68c56b5389f1583d15
Angela-K-C <168468383+Angela-K-C@users.noreply.github.com>
2025-06-26 20:58:57 +0300

    Completed the logic for uploads

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.drinksproject {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires java.desktop;
 
 
     opens com.example.drinksproject to javafx.fxml;
