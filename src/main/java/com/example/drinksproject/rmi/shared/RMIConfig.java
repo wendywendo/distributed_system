99d490468a59b5f6f4c83c07be4733c36abf7469
Angela-K-C <168468383+Angela-K-C@users.noreply.github.com>
2025-07-07 20:44:47 +0300

    Changed the IP config addresses

--- src/main/java/com/example/drinksproject/rmi/shared/RMIConfig.java
+++ src/main/java/com/example/drinksproject/rmi/shared/RMIConfig.java
@@ -1,7 +1,7 @@
 package com.example.drinksproject.rmi.shared;
 
 public class RMIConfig {
-    public static final String HOST = "192.168.1.100"; // Replace with your server IP
+    public static final String HOST = "10.5.210.76"; // Replace with your server IP
     public static final int PORT = 1099;
 
     public static String getURL(String serviceName) {
