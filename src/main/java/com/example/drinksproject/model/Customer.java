8150c4ae2f4fa6e4acd4a6f47676a17cee2199b5
Annecrafted <maryannealicia05@gmail.com>
2025-07-08 15:12:33 +0300

    Implemented Customer

--- src/main/java/com/example/drinksproject/model/Customer.java
+++ src/main/java/com/example/drinksproject/model/Customer.java
@@ -1,30 +1,41 @@
 package com.example.drinksproject.model;
 
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
 public class Customer {
-    private int id;
-    private String name;
-    private String phone;
+    private final SimpleIntegerProperty id;
+    private final SimpleStringProperty name;
+    private final SimpleStringProperty phone;
 
     public Customer(int id, String name, String phone) {
-        this.id = id;
-        this.name = name;
-        this.phone = phone;
+        this.id = new SimpleIntegerProperty(id);
+        this.name = new SimpleStringProperty(name);
+        this.phone = new SimpleStringProperty(phone);
     }
 
     public int getId() {
+       return id.get();
+    }
+
+    public SimpleIntegerProperty customerIdProperty() {
         return id;
     }
 
-    public String getName() {
+    public String getName() { return name.get(); }
+
+    public SimpleStringProperty customerNameProperty() {
         return name;
     }
 
     public String getPhone() {
+        return phone.get();
+    }
+    public SimpleStringProperty customerPhoneProperty() {
         return phone;
     }
-
     @Override
     public String toString() {
-        return name + " (" + phone + ")";
+        return getName() +"(" + getPhone()+")";
     }
 }
